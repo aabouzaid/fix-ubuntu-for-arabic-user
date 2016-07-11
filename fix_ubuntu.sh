7cbe6daed3bf6fef3ca4b53029987188ed5850af
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2016-07-11 22:51:53 +0200

    Set default Arabic font for system

--- fix_ubuntu.sh
+++ fix_ubuntu.sh
@@ -4,6 +4,7 @@
 # Vars
 
 logsFile="./fix-ubuntu.log"
+recommendedArabicFont="KacstOne"
 
 
 #########################################
@@ -54,6 +55,45 @@ mainDialog () {
 }
 
 
+# Select an Arabic font.
+#=======================
+selectArabicFont () {
+  if [[ -x ${1} ]]; then
+    defaultArabicFont="${1}"
+  else
+    defaultArabicFont="KacstOne"
+  fi
+
+  #
+  fontsList=$(fc-list :lang=ar family | sort | awk '{printf("%s;", $0)}')
+
+  #
+  origIFS=${IFS}; IFS=';'; 
+  read -ra fontsListArray <<< "${defaultArabicFont};${fontsList}"
+  IFS=${origIFS}
+
+  #
+  zenity --entry --title "Select Arabic font for system." --entry-text "${fontsListArray[@]}" --text "Please select Arabic font for ${action}"
+}
+
+
+# Set system default Arabic font.
+#================================
+setSystemArabicFont () {
+  selectedArabicFont=$(selectArabicFont ${recommendedArabicFont})
+  xmlArabicFile="69-language-selector-ar.conf"
+  xmlArabicPath="/etc/fonts/conf.avail/${xmlArabicFile}"
+  if [[ -f "${xmlArabicPath}" ]]; then
+    showZenityDialog "question" "File already exists." "The file ${xmlArabicFile} already exists, do you like to overwite it?"
+    if [[ $? -eq 0 ]]; then
+      runSudo cp -a "./files/${xmlArabicFile}" /etc/fonts/conf.avail/
+      runSudo ln -s "${xmlArabicPath}" "/etc/fonts/conf.d/${xmlArabicFile}"
+      check_exit_status
+    else
+      mainDialog
+    fi
+  fi
+}
 
 
 
@@ -67,7 +107,8 @@ do
   
   # Checking user's choice.
   case "${mainAction}" in
-    "")
+    "Set default system Arabic font.")
+      setSystemArabicFont
     ;;
     *)
       if [[ $? = -1 ]]; then
