97cff2c12effdc1137ee041b841ff5d63b8b4de3
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2016-07-15 23:18:57 +0200

    Set the height and width of main dialog dynamically

---
+++ functions/mainDialog.sh
@@ -0,0 +1,22 @@
+# Convert list of fixes variable to an array.
+# This list imported from "commonVars.sh" file.
+IFS=$'\n'
+fixesArray=( $(printf 'FALSE \n%s\n' ${fixesList}) )
+unset IFS
+
+# Set main dialog width based on max width of itmes in the fixes list.
+itemsMaxWidth=$(wc -L <<< "${fixesList}")
+mainDialogWidth=$(((${itemsMaxWidth} * 4) + 200))
+
+# Set main dialog height based on number of itmes in the fixes list.
+itemsCount=$(wc -l <<< "${fixesList}")
+mainDialogHeight=$((${itemsCount} * 20 + 160))
+
+# Main zenity dialog.
+mainDialog () {
+  mainAction=$(
+  zenity --list --radiolist --width=${mainDialogWidth} --height=${mainDialogHeight} \
+    --title="What do you want to do?" \
+    --column="  #  " --column="Action" "${fixesArray[@]}"
+  )
+}
