bb24b39c16b33f5a2a7b5c727ba13924d8c89a9b
Ahmed AbouZaid <ahmed.m@aabouzaid.com>
2016-07-16 02:33:16 +0200

    Select zenity message and more comments

--- functions/selectArabicFont.sh
+++ functions/selectArabicFont.sh
@@ -1,17 +1,26 @@
 # Select an Arabic from system.
 selectArabicFont () {
+
+  # Use 1st parameter as a dialog message.
   if [[ -x ${1} ]]; then
-    defaultArabicFont="${1}"
+    selectFontMessage="${1}"
+  else
+    selectFontMessage="Please select an Arabic font."
+  fi
+
+  # Use 2nd parameter as a default font.
+  if [[ -x ${2} ]]; then
+    defaultArabicFont="${2}"
   else
     defaultArabicFont="KacstOne"
   fi
 
-  #
+  # Get list of Arabic fonts on the system.
   fontsList=$(fc-list :lang=ar family | sort | awk '{printf("%s;", $0)}')
   IFS=';'; 
   read -ra fontsListArray <<< "${defaultArabicFont};${fontsList}";
   unset IFS
 
-  #
-  zenity --entry --title "Select Arabic font for system." --entry-text "${fontsListArray[@]}" --text "Please select Arabic font for ${action}"
+  # Ask user to select one of Arabic fonts.
+  zenity --entry --title "" --entry-text "${fontsListArray[@]}" --text "${selectFontMessage}"
 }
